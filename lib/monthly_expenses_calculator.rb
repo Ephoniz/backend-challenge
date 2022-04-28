043d2f4500f68711cefc4e842c99667263379188
Maruan Aguerdouh <maruan.mx@gmail.com>
2022-04-28 03:13:38 +0200

    should get total amounts of expenses

--- lib/monthly_expenses_calculator.rb
+++ lib/monthly_expenses_calculator.rb
@@ -4,6 +4,25 @@ def get_expenses_data(csv_directory)
   CSV.parse(File.read(csv_directory))
 end
 
+def calculate_total_expenses_amounts(expenses)
+    total_expenses_amount = {
+        transportation: 0,
+        meals: 0,
+        parking: 0
+    }
 
+    expenses.each do |expense|
+        case expense[0]
+        when 'TRANSPORTATION'
+            total_expenses_amount[:transportation] += expense[1].to_i
+        when 'MEAL'
+            total_expenses_amount[:meals] += expense[1].to_i
+        when 'PARKING'
+            total_expenses_amount[:parking] += expense[1].to_i
+        end
+    end
+
+    return total_expenses_amount
+end
 
 
--- spec/lib/monthly_expenses_calculator_spec.rb
+++ spec/lib/monthly_expenses_calculator_spec.rb
@@ -5,4 +5,13 @@ describe 'monthly_expenses_calculator' do
     it 'should get the data from a csv file' do
         expect(get_expenses_data('lib/data.csv')).not_to be_empty
     end
+
+    it 'should get total amounts of data provided' do
+        data = get_expenses_data('lib/data.csv')
+        expect(calculate_total_expenses_amounts(data)).to eq({
+            transportation: 110,
+            meals: 4,
+            parking: 35
+        })
+    end
 end
